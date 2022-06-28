a65c8210b6895dbdea57d68b04e318e97433056b
Life Zero <55802547+lacie-life@users.noreply.github.com>
2022-06-28 15:27:07 +0700

    Update smartWardrobe.ino

--- Arduino/smartWardrobe.ino
+++ Arduino/smartWardrobe.ino
@@ -31,15 +31,58 @@ void setup()
   Serial.print("Oke");
   pinMode(18, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(18), slot1, RISING);
+  attachInterrupt(digitalPinToInterrupt(19), slot2, RISING);
+  attachInterrupt(digitalPinToInterrupt(20), slot3, RISING);
+  attachInterrupt(digitalPinToInterrupt(21), slot4, RISING);
 }
 void sendRFID(String data){
   Serial.println(data);
   
 }
+//void readDistance(){
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//  // Reads the echoPin, returns the sound wave travel time in microseconds
+//  duration = pulseIn(echoPin, HIGH);
+//  // Calculating the distance
+//  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+//  // Displays the distance on the Serial Monitor
+//  if (distance <= 35){
+//    String checkpeople = "p:1";
+//    sendRFID(checkpeople);
+//  }
+//  else{
+//    String checkpeople = "p:0";
+//    sendRFID(checkpeople);
+//  }
+//}
+
 void slot1(){
   message = "";
   message += idx;
   message += "A_1:";
+  readRFID();
+}
+void slot2(){
+  message = "";
+  message += idx;
+  message += "A_2:";
+  readRFID();
+}
+void slot3(){
+  message = "";
+  message += idx;
+  message += "A_3:";
+  readRFID();
+}
+void slot4(){
+  message = "";
+  message += idx;
+  message += "A_4:";
   readRFID();
 }
 void readRFID(){
